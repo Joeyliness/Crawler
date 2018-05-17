65633f7cb3b683c9fa3eaaa7501cf323ab3db6f6
Joeyliness <joeyliness@gmail.com>
2018-05-17 22:01:48 +0800

    Update cped_nccu_edu.R

--- cped_nccu_edu.R
+++ cped_nccu_edu.R
@@ -39,12 +39,44 @@ for (i in seq_along(data)) {
     
     # get page content
     destination <- remDr$getPageSource()[[1]] %>% read_html(encoding = "UTF-8")
+    
+        
     baseinfo    <- destination %>% html_nodes(., xpath = '//table[@id="basic-info"]') %>% html_text() %>% 
-                   gsub('\t', '', .) %>% gsub('\n', ' ', .) %>% stri_trim_both() 
-    Info[[i]]   <- baseinfo
-    Sys.sleep(3)
+                   gsub('\t', '', .) %>% gsub('\n', ' ', .) %>% stri_trim_both()
+        
+        
+    name        <- destination %>% html_nodes('h1.title') %>% html_text()
+    
+        
+    degree      <- destination %>% 
+                   html_nodes(., xpath = '//div[@class="field field-name-field-re-degree field-type-list-text field-label-hidden"]') %>%
+                   html_text()
+    if (length(degree) == 0) {degree = NA}
+        
+        
+    abroadexp   <- destination %>% 
+                   html_nodes(., xpath = '//div[@class="field field-name-field-re-abroad-exp field-type-list-text field-label-hidden"]') %>%
+                   html_text()
+    if (length(abroadexp) == 0) {abroadexp = NA}
+        
+        
+    abroadcoun  <- destination %>% 
+                   html_nodes(., xpath = '//div[@class="field field-name-field-re-abroad-country field-type-list-text field-label-hidden"]') %>% 
+                   html_text()
+    if (length(abroadcoun) == 0) {abroadcoun = NA}
+        
+        
+    worktime    <- destination %>% 
+                    html_nodes(., xpath = '//div[@class="field field-name-field-re-work-time field-type-text field-label-hidden"]') %>%
+                    html_text()
+    if (length(worktime) == 0) {worktime = NA}
+        
+        
   }
+  sum  <- data.frame(name, degree, abroadexp, abroadcoun, worktime, baseinfo) 
+  Info <- rbind(Info, sum)
+  Sys.sleep(3)
 }
-
 # close Chrome
 remDr$close()
+write.csv(Info, row.names = F, 'Info.csv')
