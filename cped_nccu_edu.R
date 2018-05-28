bdf8a196299a79b159e0b00f77ecda77c609e0c4
Joeyliness <joeyliness@gmail.com>
2018-05-28 16:14:25 +0800

    Update cped_nccu_edu.R

--- cped_nccu_edu.R
+++ cped_nccu_edu.R
@@ -1,82 +1,67 @@
 library(rvest)
 library(RSelenium)
 library(stringi)
-
-# all data.csv（all Chinese names in pinyin）
-data <- read.csv(file.choose(), encoding = 'UTF-8', header = F) %>% .$V1 %>% as.vector()
-
-# start Selenium server
 shell("java -jar D:/R/library/Rwebdriver/selenium-server-standalone-3.7.1.jar", 
       wait = FALSE, invisible = FALSE)
-
-# open Chrome-connect to webpage-input each name-web crawling
-Info <- list()
 remDr <- remoteDriver(browserName = "chrome")
 remDr$open()
+
+# all data.csv（all Chinese names in pinyin）
+data <- read.csv(file.choose(), encoding = 'utf-8', header = F) %>% .$V1 %>% as.vector()
+Info <- list()
 for (i in seq_along(data)) {
   url <- 'http://cped.nccu.edu.tw/listhan'
   remDr$navigate(url)
   
-  # locate to the "input" search box
-  subElem <- remDr$findElement(using = 'name', value = 'field_re_han_name_value')
+  # Locate to the "input" search box
+  subElem <- remDr$findElement('name', 'field_re_han_name_value')
   
-  # clear default information in search box
+  # Clear default information in search box
   subElem$clearElement()
   subElem$sendKeysToElement(list(c(data[i]), key = 'enter'))
   
-  # searching with no result（show “資料庫無任何記錄”），skip to next person
-  emp <- remDr$getPageSource()[[1]] %>% read_html(encoding = "UTF-8")
+  # If there is no search results（show “資料庫無任何記錄”）, skip to next person
+  emp <- remDr$getPageSource()[[1]] %>% read_html(encoding = "utf-8")
   if (emp %>% html_nodes('div.view-empty') %>% html_text() %>% length == 1) {
     next
   } else {
     
-    # locate to the link if person information is available
+    # Locate to the link if person information is available and click
     personElem <- remDr$findElement(value = '//span[@class="field-content"]/a[@href]')
     personurl <- personElem$getElementAttribute('href')
-    
-    # click the located link
     personElem$clickElement()
     
-    # get page content
-    destination <- remDr$getPageSource()[[1]] %>% read_html(encoding = "UTF-8")
-    
-        
+    # Crawl page content
+    destination <- remDr$getPageSource()[[1]] %>% read_html(encoding = "utf-8")
     baseinfo    <- destination %>% html_nodes(., xpath = '//table[@id="basic-info"]') %>% html_text() %>% 
                    gsub('\t', '', .) %>% gsub('\n', ' ', .) %>% stri_trim_both()
-        
-        
     name        <- destination %>% html_nodes('h1.title') %>% html_text()
     
-        
     degree      <- destination %>% 
                    html_nodes(., xpath = '//div[@class="field field-name-field-re-degree field-type-list-text field-label-hidden"]') %>%
                    html_text()
     if (length(degree) == 0) {degree = NA}
-        
         
     abroadexp   <- destination %>% 
                    html_nodes(., xpath = '//div[@class="field field-name-field-re-abroad-exp field-type-list-text field-label-hidden"]') %>%
                    html_text()
     if (length(abroadexp) == 0) {abroadexp = NA}
         
-        
     abroadcoun  <- destination %>% 
                    html_nodes(., xpath = '//div[@class="field field-name-field-re-abroad-country field-type-list-text field-label-hidden"]') %>% 
                    html_text()
     if (length(abroadcoun) == 0) {abroadcoun = NA}
         
-        
     worktime    <- destination %>% 
                     html_nodes(., xpath = '//div[@class="field field-name-field-re-work-time field-type-text field-label-hidden"]') %>%
                     html_text()
     if (length(worktime) == 0) {worktime = NA}
-        
-        
+          
   }
   sum  <- data.frame(name, degree, abroadexp, abroadcoun, worktime, baseinfo) 
   Info <- rbind(Info, sum)
   Sys.sleep(3)
 }
-# close Chrome
+
 remDr$close()
 write.csv(Info, row.names = F, 'Info.csv')
